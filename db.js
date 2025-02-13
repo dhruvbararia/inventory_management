a2d2f5544ce9f9754bbe7dc3795f86e54de6d401
Dhruv Bararia <100354931+dhruvbararia@users.noreply.github.com>
2025-02-13 20:20:01 +0530

    console.log

--- db.js
+++ db.js
@@ -3,6 +3,16 @@ const fs = require("fs");
 
 async function connectDB() {
   try {
+    console.log({
+      host: "mysql-inventory-dhruv-f140.h.aivencloud.com",
+      port: 20050,
+      user: "avnadmin",
+      password: process.env.PASSWORD,
+      database: "defaultdb",
+      ssl: {
+        ca: fs.readFileSync("./ca.pem"), // Provide the correct path to CA.pem
+      },
+    });
     const db = await mysql.createConnection({
       host: "mysql-inventory-dhruv-f140.h.aivencloud.com",
       port: 20050,
