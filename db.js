946c3df30bfbfa8817ab2163b758ff7a19ae7c73
Dhruv Bararia <100354931+dhruvbararia@users.noreply.github.com>
2025-02-13 20:10:38 +0530

    createClient

--- db.js
+++ db.js
@@ -1,20 +1,25 @@
 const mysql = require("mysql2/promise"); // ✅ Use mysql2/promise
+const fs = require("fs");
 
 async function connectDB() {
-    try {
-        const db = await mysql.createConnection({
-            host: "localhost",
-            user: "root",
-            password: "",
-            database: "office_inventory",
-        });
+  try {
+    const db = await mysql.createConnection({
+      host: "mysql-inventory-dhruv-f140.h.aivencloud.com",
+      port: 20050,
+      user: "avnadmin",
+      password: process.env.password,
+      database: "defaultdb",
+      ssl: {
+        ca: process.env.ca, // Provide the correct path to CA.pem
+      },
+    });
 
-        console.log("✅ MySQL Connected!");
-        return db;
-    } catch (error) {
-        console.error("❌ Database connection failed:", error);
-        process.exit(1); // Exit on failure
-    }
+    console.log("✅ MySQL Connected!");
+    return db;
+  } catch (error) {
+    console.error("❌ Database connection failed:", error);
+    process.exit(1); // Exit on failure
+  }
 }
 
 module.exports = connectDB;
